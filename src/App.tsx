27bd65201c4e6f462abfa859b23d5bc61cd2cb98
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-31 20:51:32 +0900

    Confirm the results

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import Greetings from './Greetings'
+import Greetings from './prac/Greetings'
+import Counter from './prac/Counter'
+import MyForm from './prac/MyForm'
+import Counter2 from './prac/Counter2'
+import ReducerSample from './prac/ReducerSample'
 
 // function App() {
 const App: React.FC = () => {
-  return <Greetings name="Hello" />;
+  const onClick = (name : string) => {
+    console.log(`${name} says hello`)
+  }
+  const onSubmit = (form: {name: string; description: string}) => {
+    console.log(form)
+  }
+  return (
+  <div>
+    <Greetings name="Hello" onClick={onClick}/>
+    <Counter />
+    <MyForm onSubmit={onSubmit} />
+    <Counter2 />
+    <ReducerSample />
+  </div>
+  );
 }
 
 export default App;
