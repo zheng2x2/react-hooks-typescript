19ee5590edf75dc76664c0e25e8b7dcd2b599d81
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-31 23:42:35 +0900

    Use TOGGLE action

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import './TodoItem.css';
+import { useTodosDispatch, Todo } from '../context/TodoContext';
 
-export type TodoItemProps = {
-    todo: {
-        id: number;
-        text: string;
-        done: boolean;
-    }
+//export 
+type TodoItemProps = {
+    // todo: {
+    //     id: number;
+    //     text: string;
+    //     done: boolean;
+    // }
+    todo: Todo; //TodoContext에서 선언
 }
 
 function TodoItem( { todo } : TodoItemProps ) {
+    
+    const dispatch = useTodosDispatch();
+
+    const onToggle = () => {
+        dispatch({
+            type: 'TOGGLE',
+            id: todo.id
+        });
+    };
+
+    const onRemove = () => {
+        dispatch({
+            type: 'REMOVE',
+            id: todo.id
+        });
+    };
+    
     return (
         <li className={`TodoItem ${todo.done ? 'done' : ''}`}>
-            <span className="text">{todo.text}</span>
-            <span className="remove">( X )</span>
+            <span className="text" onClick={onToggle}>
+                {todo.text}
+            </span>
+            <span className="remove" onClick={onRemove}>( X )</span>
         </li>
     )
 }
